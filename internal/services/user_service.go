cecd9acfc8d7516f96a4a65ff475f7f8dc13d112
Burhaan <burhaanashraf@outlook.com>
2026-07-07 17:14:31 +0530

    feat: implement user registration with password hashing

---
+++ internal/services/user_service.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"context"
+	"errors"
+
+	"github.com/BurhaanAshraf/finance-api/internal/auth"
+	"github.com/BurhaanAshraf/finance-api/internal/model"
+	"github.com/BurhaanAshraf/finance-api/internal/repository"
+)
+
+type UserService struct {
+	userRepository *repository.UserRepository
+}
+
+func NewUserService(userRepository *repository.UserRepository) *UserService {
+	return &UserService{
+		userRepository: userRepository,
+	}
+}
+
+func (s *UserService) Register(ctx context.Context, name, email, password string) (*model.User, error) {
+	existingUser, err := s.userRepository.FindByEmail(ctx, email)
+
+	if err == nil && existingUser != nil {
+		return nil, errors.New("user already exists")
+	}
+
+	hashedPassword, err := auth.HashPassword(password)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &model.User{
+		Name:         name,
+		Email:        email,
+		PasswordHash: hashedPassword,
+	}
+
+	err = s.userRepository.Create(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
